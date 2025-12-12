8046f01fda2a2618e0f7fb133bd84bb998d83a24
manzitresor <manzitre@gmail.com>
2025-12-12 14:19:49 +0200

    refactor: add custom querie: derived query methods

--- src/main/java/com/codewithmosh/store/repositories/ProductRepository.java
+++ src/main/java/com/codewithmosh/store/repositories/ProductRepository.java
@@ -3,5 +3,13 @@ package com.codewithmosh.store.repositories;
 import com.codewithmosh.store.entities.Product;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends CrudRepository<Product,Long> {
+
+    List<Product> findByName(String name);
+    List<Product> findByNameContaining(String name);
+    List<Product> findByPrice(double price);
+
+    List<Product> findByNameOrderByPriceDesc(String name);
 }
