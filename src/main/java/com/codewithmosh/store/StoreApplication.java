57cd61a6bae9a1ff8dd4397c9f4296ba98b8696d
manzitresor <manzitre@gmail.com>
2025-12-17 07:36:50 +0200

    chore: templates engine

--- src/main/java/com/codewithmosh/store/StoreApplication.java
+++ src/main/java/com/codewithmosh/store/StoreApplication.java
@@ -8,7 +8,8 @@ import org.springframework.context.ApplicationContext;
 public class StoreApplication {
 
 	public static void main(String[] args) {
-	    ApplicationContext context =SpringApplication.run(StoreApplication.class, args);
+//	    ApplicationContext context =
+				SpringApplication.run(StoreApplication.class, args);
 	}
 
 }
