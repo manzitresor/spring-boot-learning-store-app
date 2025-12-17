12c3d6aff69fa68207ae936fd72331d557f308e4
manzitresor <manzitre@gmail.com>
2025-12-17 12:27:01 +0200

    chore: add dynamic route for the user

---
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -0,0 +1,32 @@
+package com.codewithmosh.store.controllers;
+
+import com.codewithmosh.store.entities.User;
+import com.codewithmosh.store.repositories.UserRepository;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Data
+@RestController
+@RequestMapping("/users")
+public class UserController {
+   private final UserRepository userRepository;
+
+   @GetMapping
+    public Iterable<User> getUsers() {
+        return  userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable long id) {
+       return userRepository.findById(id).orElse(null);
+    }
+}
